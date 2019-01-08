2da07b29dd2021fe08fd5749b4090dbeb14abe4f
Shabalin Slava <svaznoy0309@gmail.com>
2019-01-08 21:14:53 +0800

    Connection Jquery

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,4 @@
 $(function() {
-
   // $("#shopping-cart-button").click(function(event) {
   //   $("#shopping-cart").addClass("visible");
   // });
@@ -43,6 +42,39 @@ $(function() {
   //   $('select').niceSelect();
   // });
 
-  $('#product-size-selection').niceSelect();
+  $("#product-size-selection").niceSelect();
 
+  $("#product-description-size-selection").niceSelect();
+
+  $("#menu-button").click(function(event) {
+    $("#navigation-menu-container").addClass("visible");
+  });
+
+  $("#navigation-close").click(function(event) {
+    $("#navigation-menu-container").removeClass("visible");
+  });
+
+  $("#search").click(function(event) {
+    $("#search-on").addClass("visible");
+  });
+
+  $("#search-close-button").click(function(event) {
+    $("#search-on").removeClass("visible");
+  });
+
+  $("#shopping-cart-button").click(function(event) {
+    $("#information").addClass("visible");
+  });
+
+  $("#information-close").click(function(event) {
+    $("#information").removeClass("visible");
+  });
+
+  $("#sing").click(function(event) {
+    $("#sing-in").addClass("visible");
+  });
+
+  $("#sing-in-close").click(function(event) {
+    $("#sing-in").removeClass("visible");
+  });
 });
