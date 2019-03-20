6924c1dbc0efaa67939468b4c3e80e416e9ad719
Shabalin Slava <svaznoy0309@gmail.com>
2019-03-20 23:43:24 +0800

    Change cart and add animate

--- app/js/common.js
+++ app/js/common.js
@@ -86,4 +86,13 @@ $(function() {
   $(".product-like").click(function() {
     $(".product-like").toggleClass("product-like-hidden");
   });
+
+  $(document).ready(function() {
+    $(".container-content").on("click", "a", function(event) {
+      event.preventDefault();
+      var id = $(this).attr("href"),
+        top = $(id).offset().top;
+      $("body,html").animate({ scrollTop: top }, 1000);
+    });
+  });
 });
